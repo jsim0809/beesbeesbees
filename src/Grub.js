75dc257c3d766841981569c9a4f6b8c81b741bad
Jeremy Sim <59082457+jsim0809@users.noreply.github.com>
2020-02-10 11:01:26 -0800

    Implement Grub, Bee, HoneyMakerBee, ForagerBee, and RetiredForagerBee classes.

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,12 @@
 var Grub = function() {
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
 };
 
+Grub.prototype = {};
+
+Grub.prototype.constructor = Grub;
+Grub.prototype.eat = function() {
+  console.log("I ate " + this.food + ".");
+};
